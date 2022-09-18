d9bc08b30a3e320486edf684b4e28d913c708628
f-porto <felipe.portosa@gmail.com>
2022-09-18 20:51:49 -0300

    só o começo

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
-import express from "express";
-import { Router, Request, Response } from "express";
+import express from 'express'
+import { Router } from 'express'
+import dotenv from 'dotenv'
+import mountRoutes from "./routes"
 
-const app = express();
-const route = Router();
-const PORT = 3333;
+dotenv.config()
 
-app.use(express.json());
+const PORT = parseInt(process.env.PORT ?? '3333')
 
-route.get("/", (req, res) => {
-  res.sendFile('./view/index.html');
-});
+const app = express()
+const route = Router()
 
-app.use(route);
+app.use(express.json())
 
-app.listen(PORT, () => console.log('Servidor rodando na porta ', PORT));
+route.get('/', (_req, res) => {
+  res.sendFile('./view/index.html', { root: __dirname })
+})
+
+app.use(route)
+mountRoutes(app)
+
+app.listen(PORT, () => console.log('Servidor rodando na porta ', PORT))

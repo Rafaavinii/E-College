3030d4d388f313f188a9fa5dfe2e0bee5cd0763a
f-porto <felipe.portosa@gmail.com>
2022-09-18 20:56:24 -0300

    criando conexão com o banco de dados

---
+++ src/db/index.ts
@@ -0,0 +1,22 @@
+import { Pool } from 'pg'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const pool = new Pool({
+    // connectionString: process.env.uri,
+
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    user: process.env.USER,
+    port: parseInt(process.env.PGPORT!),
+    password: process.env.PASSWORD,
+
+    // ssl: {
+    //     rejectUnauthorized: false
+    // }
+});
+
+export = {
+    query: (text: string, params: any[]) => pool.query(text, params)
+};
